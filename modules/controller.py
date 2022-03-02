594e3b30da008f13f3934af40405bcc0dc2d043c
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2022-03-02 20:52:00 +0000

    Adds function for endpoint for user details retrieval

---
+++ modules/controller.py
@@ -0,0 +1,37 @@
+import os, sys
+import json
+from flask import Flask, jsonify
+import requests
+
+
+curdir = os.path.dirname(os.path.realpath(__file__))
+cpath = os.path.dirname(curdir)
+if not cpath in sys.path:
+    sys.path.append(cpath)
+
+
+def get_user(user, token)->json:
+    """
+    Takes username and github generated token and returns json on user acount details
+
+    Args:
+        user(str): github account username
+        token(str): github account token
+
+    Returns:
+        json of accounts details
+    """
+    # create authourization headers for get request
+    headers = {"Authorization":"Bearer {}".format(token)}
+    # send get request to github api
+    resp = requests.get('https://api.github.com/users/{}'.format(user), headers=headers)
+    if resp.status_code == 200:
+        # retrive response body
+        d = resp.json()
+        info_list = ['name', 'email', 'bio','followers', 'following']
+        dt = {k:d[k] for k in info_list}
+        dt["repos"] = {"public_repos": d["public_repos"], "total_private_repos":d["total_private_repos"], \
+             "owned_private_repos":d["owned_private_repos"]}
+        return jsonify(dt)
+    else:
+        return jsonify({"error":"Not Found"})
