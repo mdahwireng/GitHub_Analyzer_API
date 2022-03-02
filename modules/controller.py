c502d15349b024675c92e758f5a42f0ed2420b6b
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2022-03-02 20:53:46 +0000

    Adds function for repo_meta data retrival endpoint

--- modules/controller.py
+++ modules/controller.py
@@ -35,3 +35,62 @@ def get_user(user, token)->json:
         return jsonify(dt)
     else:
         return jsonify({"error":"Not Found"})
+
+
+def get_repo_meta(user, token)->json:
+    """
+    Takes username and github generated token and returns json of details on each repository
+
+    Args:
+        user(str): github account username
+        token(str): github account token
+
+    Returns:
+        json of details of each repository 
+    """
+    # create authourization headers for get request
+    headers = {"Authorization":"Bearer {}".format(token)}
+    # send get request to github api
+    resp = requests.get('https://api.github.com/users/{}/repos'.format(user), headers=headers)
+    if resp.status_code == 200:
+        # retrive response body
+        d = resp.json()
+        info_list = ["forks", "languages_url", "contributors_url", "branches_url"]
+        dt = {repo["name"]:{k:repo[k] for k in info_list} for repo in d}
+        for repo in dt.keys():
+            # Retrieve language details
+            languages_url = "https://api.github.com/repos/{}/{}/languages".format(user,repo)
+            languages = requests.get(languages_url, headers=headers).json()
+            total_contribs = sum([languages[l] for l in languages])
+            lang_list = [(l, float("{:.2f}".format(languages[l]/total_contribs * 100))) \
+                                for l in languages]
+            lang_list.sort(key=lambda x:x[1], reverse=True)
+            dt[repo]["languages"] = lang_list[:3]
+
+            # Retrieve branches details
+            branches_url = "https://api.github.com/repos/{}/{}/branches"
+            branches = len(requests.get(branches_url, headers=headers).json())
+            dt[repo]["branches"] = branches
+
+            # Retrieve commit activity details
+            commit_url = "https://api.github.com/repos/{}/{}/stats/commit_activity".format(user,repo)
+            commits = requests.get(commit_url, headers=headers).json()
+            dt[repo]["total_commits"] = sum([c["total"] for c in commits])
+
+            # Retrieve contributors details
+            contributors_url = "https://api.github.com/repos/{}/{}/contributors".format(user,repo)
+            contributors = requests.get(contributors_url, headers=headers).json()
+            dt[repo]["contributors"] = [c["login"] for c in contributors]
+
+            # Retrieve clones details
+            clone_url = "https://api.github.com/repos/{}/{}/traffic/clones".format(user,repo)
+            clones = requests.get(clone_url, headers=headers).json()
+            dt[repo]["clones"] = {"count": clones["count"], "uniques": clones["uniques"]}
+            
+            # Retrieve views(visitors) details
+            views_url =" https://api.github.com/repos/{}/{}/traffic/views".format(user,repo)
+            views = requests.get(views_url, headers=headers).json()
+            dt[repo]["visitors"] = {"uniques": views["uniques"], "count": views["count"]}
+        return jsonify(dt)
+    else:
+        return jsonify({"error":"Not Found"})
