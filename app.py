9ab25a5920462a667251c84a64d01c3f4ed22d2a
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2022-03-15 13:53:37 +0000

    Adds end point for named repo for python code analysis

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import json
 from flask import Flask, jsonify
 import requests
+import os
+import subprocess
+import shutil
 
 app = Flask(__name__)
 
@@ -104,6 +107,10 @@ def get_single_repo_meta(user, token, repo_name)->json:
 
     Returns:
         json of details of repository 
+        metrics :
+            cyclomatic complxity
+            raw metrics
+            maintinability Index
     """
     # create authourization headers for get request
     headers = {"Authorization":"Bearer {}".format(token)}
@@ -153,6 +160,98 @@ def get_single_repo_meta(user, token, repo_name)->json:
         return jsonify(dt)
     else:
         return jsonify({"error":"Not Found"})
+
+
+@app.route('/single_repos_pyanalysis/<string:user>/<string:token>/<string:repo_name>',methods=["GET"])
+def get_single_repo_pyanalysis(user, token, repo_name)->json:
+    """
+    Takes username, github generated token and name of repo and returns json of details of python code analis in 
+    repository
+
+    Args:
+        user(str): github account username
+        token(str): github account token
+        repo_name(str): github repository name
+
+    Returns:
+        json of details of python code analysis in repository 
+    """
+    # create authourization headers for get request
+    headers = {"Authorization":"Bearer {}".format(token)}
+    # send get request to github api
+    resp = requests.get('https://api.github.com/users/{}/repos'.format(user), headers=headers)
+    if resp.status_code == 200:
+        # retrive response body
+        d = resp.json()
+
+        # retrieve named repo
+        repo_details = [repo for repo in d if repo["name"]==repo_name]
+        
+        if repo_details:
+            
+            # check if the repo contains python files
+            if "Python" in repo_details[0]["language"]:
+
+                # retrieve clone url
+                clone_url = repo_details[0]["clone_url"]
+
+                # dir for cloned repos
+                tmp_dir = "./tmp"
+
+                # dir for named repo
+                repo_path = "{}/{}".format(tmp_dir, repo_name)
+                
+                if os.path.exists(repo_path):
+                    shutil.rmtree(tmp_dir)
+
+                if os.path.exists(tmp_dir):
+                    shutil.rmtree(tmp_dir)
+
+                os.mkdir(tmp_dir)
+                os.mkdir(repo_path)
+
+                # run cmd process to clone repo
+                process = subprocess.Popen(["git", "clone", clone_url, repo_path],
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE,
+                     universal_newlines=True)
+                stdout, stderr = process.communicate()
+
+                # if there is no error
+                if process.returncode == 0:
+                    analysis_dict = {"cyclomatic_complexity":"cc", "raw_metrics":"raw", "maintainability_index":"mi"}
+                    analysis_results = {}
+                    for k,v in analysis_dict.items():
+
+                        process = subprocess.Popen(["radon", v, repo_path, "-s", "-j"],
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.PIPE,
+                            universal_newlines=True)
+                        stdout, stderr = process.communicate()
+                        
+                        if process.returncode == 0:
+                            analysis_results[k] = json.loads(stdout.strip())
+                        else:
+                             analysis_results[k] = json.loads(stderr.strip())
+                    
+                    # delete tmp_dir after checking code metrics
+                    shutil.rmtree(tmp_dir)
+
+                    return jsonify(analysis_results)
+
+                    
+                else:
+                    return jsonify({"error" : stderr})
+
+            else:
+                jsonify({"error":"repository does not contain python files"})
+
+        else:
+            jsonify({"error":"repository not found"})
+    
+    else:
+        return jsonify({"error":"Not Found"})
+
 
 
 if __name__ == "__main__":
