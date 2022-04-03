7cf8fb51c495781bae3c66794b58fb0cc675bdc2
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2022-04-03 21:55:59 +0000

    Adds endpoint to combine retrieval of repo meta data and repo python analysis

--- app.py
+++ app.py
@@ -203,6 +203,83 @@ def get_single_repo_pyanalysis(user, token, repo_name)->json:
         return jsonify({"error":resp.json()["errors"][0]["message"]})
 
 
+@app.route('/single_repos_meta_single_repos_pyanalysis/<string:user>/<string:token>/<string:repo_name>',methods=["GET"])
+def single_repos_meta_single_repos_pyanalysis(user, token, repo_name)->json:
+    """
+    Takes username, github generated token and name of repo and returns json of details of python code analis in 
+    repository
+
+    Args:
+        user(str): github account username
+        token(str): github account token
+        repo_name(str): github repository name
+
+    Returns:
+        json of details of python code analysis in repository 
+    """
+    # create authourization headers for get request
+    headers = {"Authorization":"Bearer {}".format(token)}
+    # send get request to github api
+    resp, resp_status_code = send_get_req(_url="https://api.github.com/search/repositories?q=repo:{}/{}".format(user,repo_name), _header=headers)
+    if resp_status_code == 200:
+        # retrive response body
+        d = resp.json()
+        
+        info_list = ["forks", "languages_url", "contributors_url", "branches_url", "description", "html_url"]
+        resp_dict = {repo["name"]:{k:repo[k] for k in info_list} for repo in d["items"]}
+        
+        repo_details = [repo for repo in d["items"]]
+        if len(repo_details) == 0:
+            resp, resp_status_code = send_get_req(_url='https://api.github.com/users/{}/repos'.format(user), _header=headers)
+            if resp_status_code == 200:
+                # retrive response body
+                d = resp.json()
+                # retrieve named repo
+                repo_details = [repo for repo in d if repo["name"]==repo_name]
+
+                resp_dict = {repo["name"]:{k:repo[k] for k in info_list} for repo in d if repo["name"] == repo_name}
+                
+                if len(repo_details) == 0:
+                    return jsonify({"repo_meta":{"error":"Not Found"}, "analysis_results":{"error":"Not Found"}})
+            else:
+                return jsonify({"repo_meta":{"error":"Not Found"}, "analysis_results":{"error":"Not Found"}}) 
+
+        dt = retrieve_repo_meta(resp_json=resp_dict, headers=headers, user=user)
+
+        lang_list = ["Python"]
+    
+        # check if the repo contains python files
+        if  check_lang_exit(user=user, repo=repo_name, headers=headers, lang_list=lang_list):
+
+            stderr, return_code, additions_dict, files = run_to_get_adds_and_save_content(repo_name, repo_dict=repo_details[0], file_ext=[".py"])
+
+            # if there is no error
+            if return_code == 0:
+
+                # run analysis for python codes
+                analysis_results = run_pyanalysis()
+                # get cyclomatic complexity values for each file
+                analysis_results["cyclomatic_complexity_summary"] = get_cc_summary(analysis_results, "complexity")
+                # get file level summary for code metrics
+                analysis_results["file_level"] = get_file_level_summary(analysis_results, additions_dict)
+                # get aggregate values of code metrics for repo
+                analysis_results["repo_summary"] = get_repo_level_summary(files, analysis_results["file_level"])
+                
+                # delete repository directory after checking code metrics
+                os.chdir("../")
+                shutil.rmtree(repo_name)
+
+                return jsonify({"repo_meta":dt, "analysis_results":analysis_results})  
+            
+            else:
+                return jsonify({"repo_meta":dt, "analysis_results":{"error" : stderr}})
+
+        else:
+            return jsonify({"repo_meta":dt, "analysis_results":{"error":"repository does not contain {} files".format(lang_list)}})
+    else:
+        return jsonify({"repo_meta":{"error":resp.json()["errors"][0]["message"]}, "analysis_results":{"error":resp.json()["errors"][0]["message"]}})
+        
+
 
 
 @app.route('/single_repos_jsanalysis/<string:user>/<string:token>/<string:repo_name>',methods=["GET"])
