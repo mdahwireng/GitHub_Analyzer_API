96d816144db038ace8641823d63126ded8d80afd
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2022-06-10 21:38:57 +0000

    Adds retrieval of commit author's github username and contributors list as well as total additions and deletions on analyzed branch

--- modules/Retrieve_Commit_History.py
+++ modules/Retrieve_Commit_History.py
@@ -1,4 +1,5 @@
-from modules.api_utils import run_cmd_process
+from collections import Counter
+from modules.api_utils import run_cmd_process, send_get_req
 
 class Retrieve_Commit_History:
     """
@@ -8,16 +9,17 @@ class Retrieve_Commit_History:
             treat_details(): Treats the details of the commit history
             treat_raw(): Treats the raw data of the commit history
             treat_stat(): Treats the stats of the commit history
-            get_commit_history(): Returns a list of commit history
+            get_commit_history_and_contributors(): A dictionary containing the commit history, contributors, number of commits and number of contributors
         """
             
-    def __init__(self, branch_dict=None) -> None:
+    def __init__(self, branch_dict=None, github_dict=None) -> None:
         """
         Initialize the class.
         Returns None.
 
         Args:
             branch_dict (dict): The branch dictionary containing the default branch and the branch to be analyzed. Default is None
+            github_dict (dict): The github details dictionary containing the username and the repository name. Default is None
         """
 
         self.branch_dict = branch_dict       
@@ -31,7 +33,17 @@ class Retrieve_Commit_History:
         
         else:
             self.log = run_cmd_process(cmd_list=["git", "log", '--pretty=format:**%H##%ct##%aN##%s', "--raw", "--stat"])[0]
+        
         self.lines = self.log.split("**")
+        self.headers = None
+
+        if github_dict:
+            self.owner = github_dict['owner']
+            self.repo = github_dict['repo']
+
+            if "token" in github_dict:
+                self.headers = {"Authorization":"Bearer {}".format(github_dict['token'])}
+
 
 
     def treat_details(self, d_list) -> tuple:
@@ -99,12 +111,15 @@ class Retrieve_Commit_History:
             files__[file_n] = {"additions":additions, "deletions":deletions}
         return files__
 
-    def get_commit_history(self) -> list:
+    def get_commit_history_and_contributors(self) -> list:
         """
         Returns a list of commit history
         Returns:
-            A list of commit history
+            A dictionary containing the commit history, contributors, number of commits and number of contributors
         """
+
+        if self.owner and self.repo:
+            author_git_user_dict = {}
 
         commit_history = []
         commit_history = []
@@ -130,6 +145,46 @@ class Retrieve_Commit_History:
                     if f in raw_dict.keys():
                         raw_dict[f].update(s)
 
-                commit_dict = {"commit_sha":commit_sha,"commit_ts":commit_ts, "author":author, "message":message, "files":raw_dict}
+                raw_dict = [{"file":k, "details":v} for k,v in raw_dict.items()]
+
+                if self.owner and self.repo:
+                    if author not in author_git_user_dict.keys():
+
+                        ref_url = "https://api.github.com/repos/{}/{}/commits/{}".format(self.owner, self.repo, commit_sha)
+                        ref_r, ref_sc = send_get_req(ref_url, _header=self.headers)
+                        
+                        if ref_sc == 200:
+                            ref_json = ref_r.json()
+                            author_git_user_dict[author] = ref_json["author"]["login"]
+                        else:
+                            author_git_user_dict[author] = None
+                        
+                        github_username = author_git_user_dict[author]
+
+                    else:
+                        github_username = author_git_user_dict[author]
+                
+                else:
+                    github_username = None
+
+                commit_dict = {"commit_sha":commit_sha,"commit_ts":commit_ts, "author":author, "author_git_user":github_username, "message":message, "files":raw_dict}
+                
                 commit_history.append(commit_dict)
-        return commit_history
+            contributor_list = [c["author"] for c in commit_history]
+            contribution_count = dict(Counter(contributor_list))
+
+            addition_deletion_dict = {author:{"additions":0, "deletions":0} for author in contribution_count.keys()}
+            for c in commit_history:
+                for f in c["files"]:
+                    if "additions" in f["details"].keys():
+                        addition_deletion_dict[c["author"]]["additions"] += f["details"]["additions"]
+                    if "deletions" in f["details"].keys():
+                        addition_deletion_dict[c["author"]]["deletions"] += f["details"]["deletions"]
+
+            if self.owner and self.repo:
+                contribution_count = [ {"author":a, "author_git_user":author_git_user_dict[a], "total_commits":c, "total_additions":addition_deletion_dict[a]["additions"], "total_deletions":addition_deletion_dict[a]["deletions"]} for a,c in contribution_count.items()]
+            else:
+                contribution_count = [ {"author":a, "total_commits":c, "total_additions":addition_deletion_dict[a]["additions"], "total_deletions":addition_deletion_dict[a]["deletions"]} for a,c in contribution_count.items()]
+            
+            
+        return {"commit_history": commit_history, "contribution_count": contribution_count, "num_commits":len(commit_history), "num_contributors":len(contribution_count)}
