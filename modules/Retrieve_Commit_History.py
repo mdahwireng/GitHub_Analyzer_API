a0bcbeda6267b8e0fdf94a65f0c6f1ed8691cd37
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2022-06-13 15:55:18 +0000

    Modifies Retrive_Commit_History to handle merged and unmerged branches as well as fixing the partial import issues

--- modules/Retrieve_Commit_History.py
+++ modules/Retrieve_Commit_History.py
@@ -1,5 +1,55 @@
 from collections import Counter
-from modules.api_utils import run_cmd_process, send_get_req
+import subprocess
+import requests
+
+
+def run_cmd_process(cmd_list) -> tuple:
+    """
+    Takes a list of elements of a shell command and executes the command
+    Returns a tuple of the output, error and return code of the process.
+
+    Args:
+        cmd_list(list): list of elements of the shell command
+
+    Returns:
+        A tuple of the output, error and return code of the process
+    """
+
+    process = subprocess.Popen(cmd_list,
+                     stdout=subprocess.PIPE, 
+                     stderr=subprocess.PIPE,
+                     universal_newlines=True)
+    
+    # retrieve the output and error
+    stdout, stderr = process.communicate()
+
+    return stdout, stderr, process.returncode
+
+
+
+def send_get_req(_url, _header=None) -> tuple:
+    """
+    Sends a get request given a url and a header(optional) and returns a tuple of response and 
+    the status code
+
+    Args:
+        _url(str): url to send the request to
+        _header(dict): header to attach to the request (optional) default: None
+
+    Returns:
+        response, response status code
+    """
+    if _header:
+        resp = requests.get(_url, headers=_header)
+    else:
+        resp = requests.get(_url)
+    return resp, resp.status_code
+
+
+
+
+    
+
 
 class Retrieve_Commit_History:
     """
@@ -22,20 +72,36 @@ class Retrieve_Commit_History:
             github_dict (dict): The github details dictionary containing the username and the repository name. Default is None
         """
 
-        self.branch_dict = branch_dict       
+        self.branch_dict = branch_dict
+        self.n_commit_default_to_branch = None
+        self.merged = False       
         if branch_dict:
             self.default_branch = self.branch_dict['default']
             self.branch = self.branch_dict['branch']
             if self.branch == self.default_branch:
                 self.log = run_cmd_process(cmd_list=["git", "log", '--pretty=format:**%H##%ct##%aN##%s', "--raw", "--stat"])[0]
+                self.n_commit_default_to_branch = len(self.log.split("**"))-1
             else:
-                self.log = run_cmd_process(cmd_list=["git", "log", "{}..{}".format(self.default_branch, self.branch), '--pretty=format:**%H##%ct##%aN##%s', "--raw", "--stat"])[0]
+                log = run_cmd_process(cmd_list=["git", "log", "{}..{}".format(self.default_branch, self.branch), '--pretty=format:**%H##%ct##%aN##%s', "--raw", "--stat"])[0]
+                if log == "":
+                    self.merged = True
+                    print("\nNo unique commits found for the branch {}. The branch may have been merged with {} (default branch)\n".format(self.branch, self.default_branch))
+                    self.log = run_cmd_process(cmd_list=["git", "log", '--pretty=format:**%H##%ct##%aN##%s', "--raw", "--stat"])[0]
+                    self.n_commit_default_to_branch = len(self.log.split("**"))-1
+                else:
+                    self.log = log
         
         else:
             self.log = run_cmd_process(cmd_list=["git", "log", '--pretty=format:**%H##%ct##%aN##%s', "--raw", "--stat"])[0]
-        
+            self.n_commit_default_to_branch = len(self.log.split("**"))-1
+
         self.lines = self.log.split("**")
         self.headers = None
+
+        if not self.n_commit_default_to_branch:
+            log = run_cmd_process(cmd_list=["git", "log", '--pretty=format:**%H##%ct##%aN##%s', "--raw", "--stat"])[0]
+            self.n_commit_default_to_branch = len(log.split("**"))-1
+            
 
         if github_dict:
             self.owner = github_dict['owner']
@@ -57,7 +123,6 @@ class Retrieve_Commit_History:
         Returns:
             A tuple containing the commit sha, commit timestamp, author and message
         """
-
         details = d_list[0].split("##")
         d_ = details[0],details[1],details[2],details[3]
         return d_
@@ -121,9 +186,32 @@ class Retrieve_Commit_History:
         if self.owner and self.repo:
             author_git_user_dict = {}
 
+        if self.merged:
+            unique_commits = []
+            for l in run_cmd_process(cmd_list=["git", "log", self.branch, "--decorate", "--oneline"])[0].split("\n"):
+                if "origin" in l and self.branch not in l:
+                    break
+                if l not in unique_commits:
+                    unique_commits.append(l)
+            if "Merge" in unique_commits[-1]:
+                unique_commits.pop(-1)
+            unique_commits = [l.split(" ")[0] for l in unique_commits]
+            unique_commits_lines = []
+
+            for commit in unique_commits:
+                for line in self.lines:
+                    if commit in line:
+                        unique_commits_lines.append(line)
+                        break
+            self.lines = unique_commits_lines
+        
+        else:
+            self.lines = self.lines[1:]
+        
+
+
         commit_history = []
-        commit_history = []
-        for line in self.lines[1:]:
+        for line in self.lines:
             l_split = line.split("\n")
             if len(l_split) > 0:
                 details = []
@@ -136,40 +224,42 @@ class Retrieve_Commit_History:
                         raw.append(l)
                     if l.__contains__("|"):
                         stats.append(l)
+                if len(details) > 0:
+                    commit_sha, commit_ts, author, message = self.treat_details(details)
+                    raw_dict = self.treat_raw(raw)
+                    stats_dict = self.treat_stat(stats)
 
-                commit_sha, commit_ts, author, message = self.treat_details(details)
-                raw_dict = self.treat_raw(raw)
-                stats_dict = self.treat_stat(stats)
+                    for f,s in stats_dict.items():
+                        if f in raw_dict.keys():
+                            raw_dict[f].update(s)
 
-                for f,s in stats_dict.items():
-                    if f in raw_dict.keys():
-                        raw_dict[f].update(s)
+                    raw_dict = [{"file":k, "details":v} for k,v in raw_dict.items()]
 
-                raw_dict = [{"file":k, "details":v} for k,v in raw_dict.items()]
+                    if self.owner and self.repo:
+                        if author not in author_git_user_dict.keys():
 
-                if self.owner and self.repo:
-                    if author not in author_git_user_dict.keys():
+                            ref_url = "https://api.github.com/repos/{}/{}/commits/{}".format(self.owner, self.repo, commit_sha)
+                            ref_r, ref_sc = send_get_req(ref_url, _header=self.headers)
+                            
+                            if ref_sc == 200:
+                                ref_json = ref_r.json()
+                                author_git_user_dict[author] = ref_json["author"]["login"]
+                            else:
+                                author_git_user_dict[author] = None
+                            
+                            github_username = author_git_user_dict[author]
 
-                        ref_url = "https://api.github.com/repos/{}/{}/commits/{}".format(self.owner, self.repo, commit_sha)
-                        ref_r, ref_sc = send_get_req(ref_url, _header=self.headers)
-                        
-                        if ref_sc == 200:
-                            ref_json = ref_r.json()
-                            author_git_user_dict[author] = ref_json["author"]["login"]
                         else:
-                            author_git_user_dict[author] = None
-                        
-                        github_username = author_git_user_dict[author]
-
+                            github_username = author_git_user_dict[author]
+                    
                     else:
-                        github_username = author_git_user_dict[author]
-                
-                else:
-                    github_username = None
+                        github_username = None
 
-                commit_dict = {"commit_sha":commit_sha,"commit_ts":commit_ts, "author":author, "author_git_user":github_username, "message":message, "files":raw_dict}
-                
-                commit_history.append(commit_dict)
+                    commit_dict = {"commit_sha":commit_sha,"commit_ts":commit_ts, "author":author, "author_git_user":github_username, "message":message, "files":raw_dict}
+                    
+                    commit_history.append(commit_dict)
+
+        
             contributor_list = [c["author"] for c in commit_history]
             contribution_count = dict(Counter(contributor_list))
 
@@ -186,5 +276,4 @@ class Retrieve_Commit_History:
             else:
                 contribution_count = [ {"author":a, "total_commits":c, "total_additions":addition_deletion_dict[a]["additions"], "total_deletions":addition_deletion_dict[a]["deletions"]} for a,c in contribution_count.items()]
             
-            
-        return {"commit_history": commit_history, "contribution_count": contribution_count, "num_commits":len(commit_history), "num_contributors":len(contribution_count)}
+        return {"commit_history": commit_history, "contribution_count": contribution_count, "num_commits_on _branch":len(commit_history), "num_commits_on_default_to_branch":self.n_commit_default_to_branch, "num_contributors":len(contribution_count)}
