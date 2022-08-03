166f77b1385db0bc62bd9e19c8bda0ccf5e4861c
M. D. Ahwireng <kaaymike@hotmail.co.uk>
2022-08-03 08:37:06 +0000

    Adds docstring to class for preparing assignment submissions for analysis

--- modules/Prepare_Assignment_Submissions.py
+++ modules/Prepare_Assignment_Submissions.py
@@ -2,14 +2,46 @@ import pandas as pd
 
 
 class PrepareAssignmentDf:
-    def __init__(self, df, run_number, link_col):
-        # read csv
+    """
+    Prepares DataFrame created from assignment submissions for analysis
+    
+    methods:
+        __init__: Initializes the class with the dataframe, the run number and the link column
+        clean_link: Cleans a given link string and returns the cleaned link
+        get_username: Retrives the username from a given link.
+        get_repo_name: Retrives the repo name from a given link.
+        get_branch_name: Retrives the branch name from a given link.
+        get_df: Adds repo name, username, branch name, and run_number columns to the dataframe
+                If a filename is given, the enhanced dataframe is saved to a csv file with the filename
+    """
+    def __init__(self, df, run_number, link_col) -> None:
+        """
+        Initialize the class with the dataframe, the run number and the link column
+
+        Args:
+            df (DataFrame): Dataframe to be prepared
+            run_number (str): Run number of the analysis
+            link_col (str): Link column of the dataframe
+
+        Returns:
+            None
+
+        """
         self.df = df
         self.run_number = run_number
         self.link_col = link_col
         
 
-    def clean_link(self, link):
+    def clean_link(self, link) -> str:
+        """
+        Cleans a given link string and returns the cleaned link
+        
+        Args:
+            link (str): Link to be cleaned
+
+        Returns:
+            str: Cleaned link
+        """
         # remove trailing foward slash and .git
         if not isinstance(link, float) and len(link) > 0:
             if link.endswith(".git"):
@@ -28,7 +60,17 @@ class PrepareAssignmentDf:
         return link
 
     
-    def get_username(self, link):
+    def get_username(self, link) -> str or None:
+        """
+        Retrives the username from a given link.
+        If the link is not a valid link, returns None
+
+        Args:
+            link (str): Link to be cleaned
+
+        Returns:
+            str: Username
+        """
         if not isinstance(link, float) and len(link) > 0:
             if link.__contains__("/tree/"):
                 return link.split("/")[3]
@@ -37,7 +79,17 @@ class PrepareAssignmentDf:
             return None
 
         
-    def get_repo_name(self,link):
+    def get_repo_name(self,link) -> str or None:
+        """
+        Retrives the repo name from a given link.
+        If the link is not a valid link, returns None
+        
+        Args:
+            link (str): Link to be cleaned
+            
+        Returns:
+            str: Repo name
+        """
         if not isinstance(link, float) and len(link) > 0:
             if link.__contains__("/tree/"):
                 return link.split("/")[4]
@@ -46,7 +98,17 @@ class PrepareAssignmentDf:
             return None
 
 
-    def get_branch_name(self, link):
+    def get_branch_name(self, link)-> str or None:
+        """
+        Retrives the branch name from a given link.
+        If the link is not a valid link, returns None
+
+        Args:
+            link (str): Link to be cleaned
+
+        Returns:
+            str: Branch name
+        """
         if not isinstance(link, float) and len(link) > 0:
             if link.__contains__("/tree/"):
                 return link.split("/")[-1]
@@ -56,7 +118,18 @@ class PrepareAssignmentDf:
             return None
 
 
-    def get_df(self,filename=None):
+    def get_df(self,filename=None) -> pd.DataFrame:
+        """
+        Adds repo name, username, branch name, and run_number columns to the dataframe
+        If a filename is given, the enhanced dataframe is saved to a csv file with the filename
+        Returns the enhanced dataframe
+
+        Args:
+            filename (str): Filename of the csv file to be saved, default None
+
+        Returns:
+            pd.DataFrame: Enhanced dataframe
+        """
         
         gh_usernames = []
         gh_repo_names = []
